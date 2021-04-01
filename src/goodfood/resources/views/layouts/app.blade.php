65c4d01c4bb90d580a8b8fd05fe565e15f744297
longnguyen-1205 <ngthanhlong.z@gmail.com>
2021-04-01 13:50:27 +0900

    finish login

--- src/goodfood/resources/views/layouts/app.blade.php
+++ src/goodfood/resources/views/layouts/app.blade.php
@@ -1,83 +1,89 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" >
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/home') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" >
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/home') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <p>dep zai k 2 phai</p>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        @if(auth()->user())
+                        @php
+                        $menu=[
+                        ['route'=>'home','label'=>'ホーム'],
+                        ['route'=>'userinfo','label'=>'会員情報'],
+                        ['route'=>'home','label'=>'店舗登録'],
+                        ['route'=>'home','label'=>'登録した店舗管理'],
+                        ['route'=>'home','label'=>'気に入った店舗'],
+                        ['route'=>'home','label'=>'口コミ一覧']
+                        ];
+                        @endphp
+                        @foreach($menu as $nav)
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route($nav['route']) }}">{{ __($nav['label']) }}</a>
+                            </li>
+                        </ul>
+                        @endforeach
+                        @endif
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
                             @endif
                             
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
                             @endif
-                        @else
+                            @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
-                        @endguest
-                    </ul>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+            </nav>
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+    </body>
 </html>
---
+++ src/goodfood/resources/views/userinfo.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('会員情報') }}</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4 col-form-label text-md-right">{{ __('ユーザ名')}}
+                        </div>
+                        <div class="col-md-4 col-form-label text-md-left">{{ __(Auth::user()->username) }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-form-label text-md-right">{{ __('名前')}}
+                        </div>
+                        <div class="col-md-4 col-form-label text-md-left">{{ __(Auth::user()->name) }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス')}}
+                        </div>
+                        <div class="col-md-4 col-form-label text-md-left">{{ __(Auth::user()->email) }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-form-label text-md-right">{{ __('アカウント種別')}}
+                        </div>
+                        <div class="col-md-4 col-form-label text-md-left">{{ __(Auth::user()->type) }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 offset-md-3 mt-4">
+                            <a href="{{ route('edituserinfo') }}" ><button class="btn btn-primary">
+                            {{ __('メールアドレス、パスワード変更') }}
+                            </button>
+                        </a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
