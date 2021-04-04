f70da27cad8a45e4447c458dce15c662a199da8b
longnguyen-1205 <ngthanhlong.z@gmail.com>
2021-04-04 16:24:31 +0900

    finish search

--- src/goodfood/app/Http/Controllers/HomeController.php
+++ src/goodfood/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,63 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
+    public function index(Request $request)
+    {   
+        $sql    = "SELECT sid FROM `store`  JOIN `users` ON id=uid WHERE (storename like '%$request->key%' OR name like '%$request->key%')";
+        
+
+        //checkbox
+        $role=["和食","洋食","中華料理","焼肉","麺料理","カフェ","その他"];
+        foreach($role as $in){
+            if($request->input($in)){
+                $check[]=$in;
+            }
+        }
+        if(isset($check)){
+            if($request->isor==1){
+           $c= "('".implode("','",$check)."')";
+            $sql.=" AND sid in (SELECT sid FROM `role` where role in  $c )";
+        }else{
+            foreach($check as $c){
+                $sql.=" AND sid in (SELECT sid FROM `role` where role = '$c' )";
+            }
+        }
+        }
+
+        //select 
+        if(!$request->sec || $request->sec==1){
+            $sql.="ORDER BY storename";
+        }
+        if($request->sec==2){
+            $sql="SELECT  IFNULL(ROUND(AVG(votes),1),0) vote , COUNT(votes) sum ,sid FROM `storecmt` RIGHT JOIN store USING(sid)  GROUP BY sid 
+            HAVING sid in"."($sql) ORDER BY vote DESC";
+        }
+
+        if($request->sec==3){
+            $sql="SELECT COUNT(a.uid) sum,sid from `storelike` a RIGHT JOIN store b USING(sid) GROUP BY sid HAVING sid in"."($sql) ORDER BY sum DESC";
+        }
+         
+        
+
+        $takesid   = DB::select($sql);
+        $numberofid = count($takesid);
+        
+        foreach( $takesid as $order){
+        $sql    = "SELECT * FROM `store`  JOIN `users` ON id=uid WHERE $order->sid=sid";
+        $store[]   = DB::select($sql);
+        $sql    = "SELECT COUNT(*) sum from `storelike` WHERE $order->sid=sid";
+        $like[]   = DB::select($sql);
+        $sql    = "SELECT  IFNULL(ROUND(AVG(votes),1),0) vote , COUNT(votes) sum FROM `storecmt` RIGHT JOIN store USING(sid) WHERE $order->sid=sid GROUP BY sid";
+        $vote[]   = DB::select($sql);
+       $sql    = "SELECT role AS s FROM `role` WHERE $order->sid=sid";
+        $type[] = DB::select($sql);
+        }
+        if($numberofid>0){
+        return view('home',compact('numberofid','store','like','vote','type','request'));
+        //return $takesid;
+        }else{
+        return view('home',compact('numberofid','request'));
+        }
+        
     }
 }
--- src/goodfood/resources/views/home.blade.php
+++ src/goodfood/resources/views/home.blade.php
@@ -1,23 +1,84 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('お店をさがす') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <nav class="navbar navbar-light bg-light justify-content-between">
+                    <a class="navbar-brand">お店を探す</a>
+                    <form class="form-inline" method="GET" action="/home">
+                        <input class="form-control mr-sm-2" type="search" name="key" placeholder="検索" aria-label="Search"
+                        value="{{ $request->key }}">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索</button>
+                        <a href="home/"><button type="button"class="btn btn-sm btn-outline-secondary">取消</button></a>
+                    </nav>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12 mb-4">
+                            <select class="form-select mb-3 btn-block btn-outline-secondary btn" name="sec">
+                                <option value=1  @if($request->sec==1 ) selected @endif >名前順</option>
+                                <option value=2 @if($request->sec==2 ) selected @endif >評価順</option>
+                                <option value=3 @if($request->sec==3 ) selected @endif >気に入った順</option>
+                            </select>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                            @php
+                            $role=["和食","洋食","中華料理","焼肉","麺料理 ","カフェ","その他 "];
+                            @endphp
+                            <div class="col-md-4">
+                                @foreach(  $role as $check)
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" name={{ $check }} value=1
+                                    @if($request->$check)checked="" @endif>
+                                    {{ $check }}
+                                    </input>
+                                </label>
+                                @endforeach
+                            </div>
+                            <div class="col-md-8 text-right">
+                                <div class="radio">
+                                    <label><input type="radio" name="isor" value=0 
+                                    @if($request->isor == 0)checked="" @endif >全て一致</label>
+                                </div>
+                                <div class="radio">
+                                    <label><input type="radio" name="isor" value=1
+                                        @if($request->isor == 1 ) checked @endif>一部一致</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                {{ $numberofid }}件
+                </div>
+                <div class="card-footer ">
+                </div>
+                @for($i=0;$i<$numberofid;$i++)
+                <div class="card mt-4">
+                <div class="card-header">
+                    <a href="/storeinfo/{{ $store[$i][0]->sid }}">{{ __( $store[$i][0]->storename ) }}</a>
+                </div>
+                <div class="card-body">
+                   <div>{{ $store[$i][0]->name }} </div>
+                   <div>{{ $like[$i][0]->sum }}like </div>
+                   <div>{{ $vote[$i][0]->vote }}point in {{ $vote[$i][0]->sum }}vote  </div>
+                   <div>
+                    @foreach($type[$i] as $a)
+                    {{ $a->s }}-
+                    @endforeach
+                   </div>
+                </div>
+                <div class="card-footer ">
                 </div>
             </div>
+            @endfor
+
+
+
+
+
+            </div>
+
+
+            
         </div>
     </div>
-</div>
-@endsection
+    @endsection
